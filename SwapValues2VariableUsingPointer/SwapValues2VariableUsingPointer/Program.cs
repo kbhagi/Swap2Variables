3cac5531bdf65532e3d07079fd3ac603e67a775a
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 19:32:57 +0530

    Add files via upload

---
+++ SwapValues2VariableUsingPointer/SwapValues2VariableUsingPointer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwapValues2VariableUsingPointer
+{
+    class Program
+    {
+        static void SwapNum(ref int x, ref int y)
+        {
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 500;
+            Console.WriteLine("Value of a and b before swapping");
+            Console.WriteLine();
+            Console.WriteLine("a = " + " " + a);
+            Console.WriteLine("b = " + " " + b);
+            SwapNum(ref a, ref b);
+            Console.WriteLine();
+            Console.WriteLine("Value of a and b after swapping");
+            Console.WriteLine();
+            Console.WriteLine("a = " + " " + a);
+            Console.WriteLine("b = " + " " + b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SwapValues2VariableUsingPointer/SwapValues2VariableUsingPointer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SwapValues2VariableUsingPointer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwapValues2VariableUsingPointer")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17cd06b2-2d8c-4704-824b-0c7945922932")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
